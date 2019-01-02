ef89f8862aa91a54900b55d13b8475e2df817502
Michael Pope <popemray1986@gmail.com>
2019-01-02 22:02:37 +0000

    Automatically backed up by Learn

--- src/components/TwitterMessage.js
+++ src/components/TwitterMessage.js
@@ -1,10 +1,11 @@
 import React from "react";
 
 class TwitterMessage extends React.Component {
-  constructor() {
-    super();
+  constructor(props) {
+    super(props);
 
-    this.state = {value: ''};
+    this.state = {value: '',
+  this.props.maxChars};
   }
 
   handleChange = e => {
